1c47fa2fbd2074c6656a4001c2cb5c7df516aa59
TNarido <tristannarido@gmail.com>
2024-02-18 13:01:35 +0800

    Added Certificate Page

--- resources/js/Components/Pages/MyAccount.vue
+++ resources/js/Components/Pages/MyAccount.vue
@@ -38,7 +38,8 @@
                             <p>Password:</p>
                             <span>••••••••••</span>
                             <span class="toggle-password" @click="togglePasswordInfo">
-                                <i><font-awesome-icon :icon="['fa-solid', passwordShow ? 'fa-eye-slash' : 'fa-eye']" /></i>
+                                <i style="color: #7AA5D2"><font-awesome-icon
+                                        :icon="['fa-solid', passwordShow ? 'fa-eye-slash' : 'fa-eye']" /></i>
                             </span>
                         </div>
                     </div>
@@ -99,7 +100,7 @@
                             <input v-if="!showPassword" type="password" required v-model="password" class="form-control">
                             <input v-else type="text" required v-model="password" class="form-control">
                             <span class="toggle-password" @click="togglePassword">
-                                <i class="eye position-absolute end-0 top-70 translate-middle-y">
+                                <i class="eye position-absolute end-0 top-70 translate-middle-y" style="color: #7AA5D2">
                                     <font-awesome-icon :icon="['fa-solid', showPassword ? 'fa-eye-slash' : 'fa-eye']" />
                                 </i>
                             </span>
@@ -109,7 +110,7 @@
                             <input v-if="!showPassword" type="password" required v-model="newPassword" class="form-control">
                             <input v-else type="text" required v-model="newPassword" class="form-control">
                             <span class="toggle-password" @click="togglePassword">
-                                <i class="eye position-absolute end-0 top-70 translate-middle-y">
+                                <i class="eye position-absolute end-0 top-70 translate-middle-y" style="color: #7AA5D2">
                                     <font-awesome-icon :icon="['fa-solid', showPassword ? 'fa-eye-slash' : 'fa-eye']" />
                                 </i>
                             </span>
@@ -213,7 +214,6 @@ function togglePasswordInfo() {
     height: 51vh;
     border-radius: 10px;
     text-align: center;
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
 }
 
 .about-info .about-text {
